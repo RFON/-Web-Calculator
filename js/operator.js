210e65e86fc9e168acaf77b9de0ceece8777241b
Fon <1090737707@qq.com>
2017-05-01 03:28:55 +0800

    operator-view finish

--- js/operator.js
+++ js/operator.js
@@ -1,21 +1,33 @@
-var number = document.getElementsByClassName("number-button")
+var numberButton = document.getElementsByClassName("number-button")
 var display = document.getElementById("screen")
-var operator = ''
+var pNode = display.getElementsByTagName("p")
+var oppositeButton = document.getElementById("opposite-button")
+var oppositeFlag = false
+var opposite
+var firstNumber
 window.onload = function() {
-    for (var i = 0; i < number.length; i++) {
-        number[i].addEventListener('click', function() {
+    for (var i = 0; i < numberButton.length; i++) {
+        numberButton[i].addEventListener('click', function() {
             var num = this.getElementsByTagName("p")[0].innerHTML
-            var pNode = display.getElementsByTagName("p")
             var initial = pNode[0].innerHTML
-            console.log(initial)
-                // var result = num.replace(/<[^>]+>/g, "")
-                // console.log(result)
             if (initial == "0" && num != ".") {
                 return
+            } else if (num == "±") {
+                oppositeFlag = !oppositeFlag
+                if (oppositeFlag == true) {
+                    opposite = '-'
+                    opposite += pNode[0].innerHTML
+                    console.log("-")
+                } else {
+                    console.log("+")
+                    opposite = pNode[0].innerHTML.match(/([0-9]+)(.)?([0-9]*)/)[0]
+                }
+                pNode[0].innerHTML = opposite
+            } else if (num == '.' && (initial.split(".").length == 2 || initial == '')) {
+                return;
             } else {
                 pNode[0].innerHTML += num
             }
-            console.log(display.getElementsByTagName("p")[0].innerHTML)
         })
     }
 }
