2b684c9479cfa52fab0bdb08c938cfa1f2ea558e
Fon <1090737707@qq.com>
2017-05-02 23:31:44 +0800

    probably finish var2.0

--- js/operator.js
+++ js/operator.js
@@ -5,23 +5,20 @@ var oppositeButton = document.getElementById("opposite-button")
 var oppositeFlag = false
 var opposite
 var firstNumber
-var secondNumber
+var secondNumber = ""
+var stack = ["", "", ""]
 var result
 var doubleOperator = ""
 var singleOperator = ""
 var operatorFlag = false
 var resultConfirm = false
-window.onload = function() {
+window.onload = function () {
     //数字、小数点、正负号按钮添加点击事件
     for (let i = 0; i < numberButton.length; i++) {
-        numberButton[i].addEventListener('click', function() {
-            if (operatorFlag == true) {
-                pNode[0].innerHTML = ""
-                operatorFlag = false
-            }
+        numberButton[i].addEventListener('click', function () {
             var num = this.getElementsByTagName("p")[0].innerHTML
             var initial = pNode[0].innerHTML
-                //首个数字为0的时候只能跟小数点
+            //首个数字为0的时候只能跟小数点
             if (initial == "0" && num != ".") {
                 return
             }
@@ -42,18 +39,27 @@ window.onload = function() {
             else if (num == '.' && (initial.split(".").length == 2 || initial == '')) {
                 return;
             } else {
+                if (operatorFlag == true) {
+                    pNode[0].innerHTML = ""
+                    operatorFlag = false
+                    secondNumber += num
+                }
                 pNode[0].innerHTML += num
+                if (resultConfirm == true) {
+                    doubleOperator = ""
+                    console.log("ok")
+                }
             }
         })
     }
-    document.getElementById("back").addEventListener('click', function() {
+    document.getElementById("back").addEventListener('click', function () {
         var backArray = ""
         for (let i = 0; i < pNode[0].innerHTML.length - 1; i++) {
             backArray += pNode[0].innerHTML[i]
         }
         pNode[0].innerHTML = backArray
     })
-    document.getElementById("clear").addEventListener('click', function() {
+    document.getElementById("clear").addEventListener('click', function () {
         pNode[0].innerHTML = ""
         firstNumber = 0
         secondNumber = 0
@@ -62,13 +68,17 @@ window.onload = function() {
         operatorFlag = false
         oppositeFlag = false
     })
-    document.getElementById("result").addEventListener('click', function() {
-            doubleOperation("=")
-        })
-        //操作符处理
-    var doubleOperation = function(operator) {
+    document.getElementById("result").addEventListener('click', function () {
+        doubleOperation("=")
+    })
+    //操作符处理
+    var doubleOperation = function (operator) {
+        if (operator == "pi") {
+            secondNumber = "pi"
+            pNode[0].innerHTML = 3.1415926535898
+            return
+        }
         if (doubleOperator == "") {
-            console.log(pNode[0].innerHTML)
             if (pNode[0].innerHTML == "") {
                 return
             } else {
@@ -76,9 +86,9 @@ window.onload = function() {
             }
             doubleOperator = operator
             operatorFlag = true
-            console.log(typeof(firstNumber))
         } else {
-            if (secondNumber !== "") {
+            console.log(secondNumber)
+            if (secondNumber != "") {
                 if (pNode[0].innerHTML == "") {
                     return
                 } else {
@@ -96,7 +106,6 @@ window.onload = function() {
                         {
                             result = firstNumber - secondNumber
                             pNode[0].innerHTML = result
-                            console.log(typeof(result))
                             firstNumber = result
                             break
                         }
@@ -104,7 +113,6 @@ window.onload = function() {
                         {
                             result = firstNumber * secondNumber
                             pNode[0].innerHTML = result
-                            console.log(typeof(result))
                             firstNumber = result
                             break
                         }
@@ -112,7 +120,6 @@ window.onload = function() {
                         {
                             result = firstNumber / secondNumber
                             pNode[0].innerHTML = result
-                            console.log(typeof(result))
                             firstNumber = result
                             break
                         }
@@ -128,51 +135,84 @@ window.onload = function() {
                 if (operator == "=") {
                     resultConfirm = true
                 }
+                if (operator != "=") {
+                    resultConfirm = false
+                }
                 operatorFlag = true
-                console.log(doubleOperator)
+                secondNumber = ""
             } else {
                 doubleOperator = operator
-                console.log(doubleOperator)
                 return
             }
         }
     }
-    var singleOperation = function(operator) {
+    var singleOperation = function (operator) {
         switch (operator) {
             case "sqrt":
                 {
-                    result = Math.sqrt(pNode[0].innerHTML)
-                    pNode[0].innerHTML = result
+                    if (pNode[0].innerHTML == "") {
+                        return
+                    }
+                    if (parseInt(pNode[0].innerHTML) < 0) {
+                        pNode[0].innerHTML = "负数没有平方根"
+                    } else {
+                        result = Math.sqrt(pNode[0].innerHTML)
+                        pNode[0].innerHTML = result
+                    }
                     break
                 }
             case "sqrt3":
                 {
-                    result = Math.pow(pNode[0].innerHTML, 1 / 3)
+                    if (pNode[0].innerHTML == "") {
+                        return
+                    }
+                    result = Math.cbrt(pNode[0].innerHTML)
                     pNode[0].innerHTML = result
                     break
                 }
             case "pow2":
                 {
+                    if (pNode[0].innerHTML == "") {
+                        return
+                    }
                     result = Math.pow(pNode[0].innerHTML, 2)
                     pNode[0].innerHTML = result
                     break
                 }
-            case "pi":
-                {
-                    pNode[0].innerHTML = 3.1415926535898
-                    break
-                }
             case "factorial":
                 {
-                    result = factorial(pNode[0].innerHTML)
-                    pNode[0].innerHTML = result
+                    if (pNode[0].innerHTML == "") {
+                        return
+                    }
+                    if (parseInt(pNode[0].innerHTML) < 0) {
+                        pNode[0].innerHTML = "负数没有阶乘"
+                    }
+                    else if (pNode[0].innerHTML.split(".").length == 2) {
+                        pNode[0].innerHTML = "非整数没有阶乘"
+                    }
+                    else {
+                        result = factorial(pNode[0].innerHTML)
+                        pNode[0].innerHTML = result
+                    }
+                    break
+                }
+            case "log2":
+                {
+                    if (pNode[0].innerHTML == "") {
+                        return
+                    }
+                    if (parseInt(pNode[0].innerHTML) <= 0) {
+                        pNode[0].innerHTML = "对数必须是正数"
+                    } else {
+                        result = Math.log2(pNode[0].innerHTML)
+                        pNode[0].innerHTML = result
+                    }
                     break
                 }
         }
         operatorFlag = true
-
     }
-    var factorial = function(itself) {
+    var factorial = function (itself) {
         var total
         if (itself < 0) {
             return 0
@@ -183,34 +223,37 @@ window.onload = function() {
         }
         return total
     }
-    document.getElementById("addition").addEventListener('click', function() {
+    document.getElementById("addition").addEventListener('click', function () {
         doubleOperation("addition")
     })
-    document.getElementById("subtraction").addEventListener('click', function() {
+    document.getElementById("subtraction").addEventListener('click', function () {
         doubleOperation("subtraction")
     })
-    document.getElementById("multiplication").addEventListener('click', function() {
+    document.getElementById("multiplication").addEventListener('click', function () {
         doubleOperation("multiplication")
     })
-    document.getElementById("division").addEventListener('click', function() {
+    document.getElementById("division").addEventListener('click', function () {
         doubleOperation("division")
     })
-    document.getElementById("mod").addEventListener('click', function() {
+    document.getElementById("mod").addEventListener('click', function () {
         doubleOperation("mod")
     })
-    document.getElementById("sqrt").addEventListener('click', function() {
+    document.getElementById("sqrt").addEventListener('click', function () {
         singleOperation("sqrt")
     })
-    document.getElementById("sqrt3").addEventListener('click', function() {
+    document.getElementById("sqrt3").addEventListener('click', function () {
         singleOperation("sqrt3")
     })
-    document.getElementById("pow2").addEventListener('click', function() {
+    document.getElementById("pow2").addEventListener('click', function () {
         singleOperation("pow2")
     })
-    document.getElementById("pi").addEventListener('click', function() {
-        singleOperation("pi")
+    document.getElementById("pi").addEventListener('click', function () {
+        doubleOperation("pi")
     })
-    document.getElementById("factorial").addEventListener('click', function() {
+    document.getElementById("factorial").addEventListener('click', function () {
         singleOperation("factorial")
+    })
+    document.getElementById("log2").addEventListener('click', function () {
+        singleOperation("log2")
     })
 }
